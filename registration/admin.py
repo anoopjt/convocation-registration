63068943b6e25a1091129aa32154083e90f44674
Dinesh Vikram V <astrodini@gmail.com>
2021-06-07 21:22:37 +0530

    Added admin interface to manage convocation registrations

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class Student(admin.ModelAdmin):
+    list_display = [field.name for field in models.Student._meta.get_fields() if field.name != 'id']
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_module_permission(self, request, obj=None):
+        return True
+
+    def get_queryset(self, request):
+        queryset = super(Student, self).get_queryset(request)
+        return queryset.exclude(id__lt=46)
+
+admin.register(models.Student)(Student)
